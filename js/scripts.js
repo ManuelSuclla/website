71679e93503ae974a19f68c3a77901f3637e58dc
ManuelSuclla <msuclla@umich.edu>
2017-10-20 16:26:38 -0400

    new Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
+
 });
+
+$("#about-me-top").click(function() {
+$('html,body').animate({
+    scrollTop: $("#About").offset().top},
+    'slow');
+});
+
+$(window).scroll(function() {
+    $(".parallax-1").css({
+    'opacity': 1.0 - (($(this).scrollTop()) / 600.0)
+    });
+
+    $(".container-1").css({
+    'opacity': (($(this).scrollTop()) / 500.0)
+    });
+});
+
+
+// var s = $(window).scrollTop(),
+// opacityValue = (s/1000.0);
+// console.log(1.0 - opacityValue);
+// $('.parallax-1.blurred-img').css('opacity', 1.0 - opacityValue);
+
+// $(".parallax-1-blur").css({"-webkit-filter": "blur("+blurValue+"px)","filter": "blur("+blurValue+"px)" })
