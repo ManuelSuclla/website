477606ddcd3ca0939c2b151b3545347093db2ee3
ManuelSuclla <msuclla@umich.edu>
2017-12-04 14:47:44 -0500

    Updated contact div and portfolio area

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-    // $("#about-me-top").click(function() {
-    // $('html,body').animate({
-    //     scrollTop: $("#About").offset().top},
-    //     'slow');
-    // });
 
     $('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -20,16 +15,5 @@ $(document).ready(function() {
         'opacity': 1.0 - (($(this).scrollTop()) / 600.0)
         });
 
-        // $(".container-1").css({
-        // 'opacity': (($(this).scrollTop()) / 500.0)
-        // });
     });
 });
-
-
-// var s = $(window).scrollTop(),
-// opacityValue = (s/1000.0);
-// console.log(1.0 - opacityValue);
-// $('.parallax-1.blurred-img').css('opacity', 1.0 - opacityValue);
-
-// $(".parallax-1-blur").css({"-webkit-filter": "blur("+blurValue+"px)","filter": "blur("+blurValue+"px)" })
