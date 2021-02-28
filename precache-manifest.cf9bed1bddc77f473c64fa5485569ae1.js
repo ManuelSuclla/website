8ac2ccf5099be1fa2d24f34b521ba2267124ba32
Manuel Suclla <msuclla@umich.edu>
2021-02-28 18:25:30 -0500

    Updates

---
+++ precache-manifest.cf9bed1bddc77f473c64fa5485569ae1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4080ad860e26ed012c669afd3ca5ae8c",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "2936aa172d6a8b17b0d4",
+    "url": "/website/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "aa85e247aad77478442f",
+    "url": "/website/static/css/main.f02bf60f.chunk.css"
+  },
+  {
+    "revision": "2936aa172d6a8b17b0d4",
+    "url": "/website/static/js/2.b859427c.chunk.js"
+  },
+  {
+    "revision": "627615a99f103ad0cd9f9078062443a0",
+    "url": "/website/static/js/2.b859427c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa85e247aad77478442f",
+    "url": "/website/static/js/main.0a2cac9c.chunk.js"
+  },
+  {
+    "revision": "29db59fd0d388444716f",
+    "url": "/website/static/js/runtime-main.b656aa90.js"
+  },
+  {
+    "revision": "80947c39fc05793bc07c4bb6239940c1",
+    "url": "/website/static/media/Resume.80947c39.pdf"
+  },
+  {
+    "revision": "f6e2be83d6a1ed2402e626ed1475a008",
+    "url": "/website/static/media/profile.f6e2be83.jpg"
+  }
+]);
